076147d24c79a3da6c2803b7d4e865cc2d8b9cb6
gfliberato <gfliberato@yahoo.com.br>
2019-06-30 15:52:25 -0300

    mensagem de boa noite adicionada

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ola mundo . Boa noite!!");
+		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 
 	}
 
